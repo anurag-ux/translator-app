0351489673cb0b5f0594c4fd4c71e74cc0d0a70d
anurag-ux <anurag.s@refactor.academy>
2023-04-28 06:05:36 +0530

    fixed CORS error

--- server/app.js
+++ server/app.js
@@ -5,6 +5,13 @@ let translateRoute = require('./routes/translate');
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Homepage')
 });
